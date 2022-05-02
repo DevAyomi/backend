8a329b0460d0ae6358b528b29de7943038b369e0
Gbolagade winner <gbolagadewinner@gmail.com>
2022-05-02 08:22:08 -0400

    login implemented successfully

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
 use App\Models\User;
 use App\Http\Resources\User as UserResource;
 
@@ -29,5 +30,21 @@ class AuthController extends Controller
             ],
        ]);
 
+    }
+
+    public function login(LoginRequest $request){
+        if(!$token = auth()->attempt($request->only('email', 'password'))){
+         return response()->json([
+            'errors' =>[
+                'email' => ['Sorry we cant find you with those details'],
+            ]
+         ]);
+        };
+
+        return (new UserResource($request->user()))->additional([
+            'meta' => [
+                'token' => $token,
+            ],
+        ]);
     }
 }
