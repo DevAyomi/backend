4ab82c3460234ec829587c2f6c8997945541d036
Gbolagade winner <gbolagadewinner@gmail.com>
2022-05-02 05:08:16 -0400

    Jwt completely set up

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,16 +7,29 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function getJWTIdentifier()
+    {
+        //Return the primary key of the user -- user id
+        return $this->getKey();
+    }
+
     /**
-     * The attributes that are mass assignable.
+     * Return a key value array, containing any custom claims to be added to the JWT.
      *
-     * @var array<int, string>
+     * @return array
      */
+    public function getJWTCustomClaims()
+    {
+        //return a key value array containing any custom claims to be added to JWT
+        return [];
+    }
+
     protected $fillable = [
         'name',
         'email',
